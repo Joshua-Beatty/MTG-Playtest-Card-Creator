0ce43c3c082c57fbc0ae05c6fd56634f83984afd
Joshua Beatty <joshuatb6@gmail.com>
2024-07-09 16:11:03 -0600

    updating build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+import { dependencies } from "./package.json";
+function renderChunks(deps: Record<string, string>) {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+  plugins: [react(), splitVendorChunkPlugin()],
+});
